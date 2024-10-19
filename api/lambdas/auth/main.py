4777ad50e2db0e56455fc29499e2045f9814cb27
Buli <agustindaguzan@gmail.com>
2024-10-19 16:32:57 -0300

    simple fastapi lambda

--- api/lambdas/auth/main.py
+++ api/lambdas/auth/main.py
@@ -1,8 +1,11 @@
-# lambda-python/main.py
-import json
+from fastapi import FastAPI, Path, Query
+from mangum import Mangum
+from typing import Optional, List
 
-def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Python Lambda!')
-    }
+app = FastAPI()
+handler = Mangum(app)
+
+
+@app.get("/", tags=["Root"])
+def read_root():
+    return {"message": "Welcome to the FastAPI demo!"}
